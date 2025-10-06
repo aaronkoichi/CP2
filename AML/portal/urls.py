354b02c2724c257055522f491c7b9062ba08dbb8
aaronkoichi <aaronlee4399@gmail.com>
2025-10-06 16:57:51 +0800

    added dummy dashboard for now.

--- AML/portal/urls.py
+++ AML/portal/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("", views.index, name="index")
+	path("", views.index, name="index"),
+	path("AML/main", views.main_page, name="main_page"),
 ]
--- AML/portal/views.py
+++ AML/portal/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 def index(request):
 	return HttpResponse("AML Portal.");
 
+
+def main_page(request):
+
+    return render(request, "main.html")
